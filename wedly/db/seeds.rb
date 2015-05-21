f203654cd92febbcce53062cdb6bffa78cf014b9
Iva Delic <ms.iva.delic@gmail.com>
2015-05-21 00:05:43 -0400

    Index page for all events works

--- wedly/db/seeds.rb
+++ wedly/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+12.times do |e|
+e = Event.create!({
+      :address_line_1 => Faker::Address.street_address,
+      :address_line_2 => Faker::Address.secondary_address,
+      :city => Faker::Address.city,
+      :province => Faker::Address.state_abbr,
+      :zip => Faker::Address.zip_code,
+      :country => Faker::Address.country
+    }
+  )
+end
