d8624d3c1b750acfefc743ca1a7f7245aa69a6f0
Melissa D'Agruma <mdagruma@gmail.com>
2015-05-20 22:18:44 -0400

    added guest controller

--- wedly/app/controllers/guests_controller.rb
+++ wedly/app/controllers/guests_controller.rb
@@ -1,36 +1,47 @@
 class GuestController < ApplicationController
 
   def index
-    @guest = Guest.all
+    @guests = Guest.all
   end
 
   def new
-
+    @guest = Guest.new
   end
 
   def edit
-
+    @guest = Guest.find
   end
 
   def show
-
+    @guest = Guest.find(params[:id])
   end
 
   def create
-
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      redirect_to guests_url
+    else
+      render :new
+    end
   end
 
   def update
-
+    @guest = Guest.new(params[:id])
+    if @guest.update_attributes(guest_params)
+      redirect_to guest_path(@guest)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @guest = Guest.find(params[:id])
+    @guest.destroy
+    flash[:notice] = "Guest #{guest.first_name} #{guest.last_name} has been deleted."
   end
 
   private
   def guest_params
-
+    params.require(:guest).permit(:first_name, :last_name, :container_id, :food_choice, :food_restrictions, :rsvp, :user_id)
   end
-
 end
