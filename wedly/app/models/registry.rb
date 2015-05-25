a8a8eb7af756e46bb6e4e81613ec08667af93f0e
Iva Delic <ms.iva.delic@gmail.com>
2015-05-25 17:17:53 -0400

    Added a link_to link to registry

--- wedly/app/models/registry.rb
+++ wedly/app/models/registry.rb
@@ -1,3 +1,12 @@
 class Registry < ActiveRecord::Base
   belongs_to :wedding
+  before_validation :smart_add_url_protocol
+
+protected
+
+  def smart_add_url_protocol
+    unless self.link_to_registry[/^https?:\/\//]
+      self.link_to_registry = "http://#{self.link_to_registry}"
+    end
+  end
 end
